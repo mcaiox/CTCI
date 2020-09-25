88d58b7c3b298ca14e7558b3925d87b27631a373
mcaiox <mcaiox@icloud.com>
2020-09-25 15:44:13 +0100

    added isUnique solution in csharp

---
+++ 1 - Arrays and Strings/isUnique.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ctci
+{
+    class isUnique
+    {
+        public static bool isUniqueChars(String str)
+        {
+            if(str.Length > 256)
+            {
+                return false;
+            }
+
+            bool[] values = new bool[256]; //create an array 0-256 where each element is initialised to false.
+
+            foreach(char c in str)
+            {
+                //c is the ascii number equiv
+                //Console.WriteLine(values[c]);
+                if (values[c]) // for each char, we check the values array and if it has seeen it before it will return true.
+                {
+                    return false;
+                }
+                values[c] = true;
+
+            }
+            return true;
+
+
+
+        }
+
+    }
+
+    class program : isUnique
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isUniqueChars("hello"));
+        }
+    }
+}
